1fd49c4b698a3b6afb638a8c9d308d7070be448d
fluffy-unicorn-tamer <luan.oelsner@outlook.de>
2024-11-28 13:27:01 +0100

    init vite+svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Welcome to uniii's website</h1>
+  <p>This site is still under construction. Meanwhile check out my socials blow.</p>
+  <a href="https://www.twitch.tv/uniiilive" target="_blank" rel="noreferrer">Twitch</a>
+  <a href="https://www.twitch.tv/uniiilive" target="_blank" rel="noreferrer">YouTube</a>
+  <a href="https://www.twitch.tv/uniiilive" target="_blank" rel="noreferrer">TikTok</a>
+  <a href="https://www.twitch.tv/uniiilive" target="_blank" rel="noreferrer">Join my discord server</a>
+</main>
+
+<style>
+a {
+    display: block
+}
+</style>
